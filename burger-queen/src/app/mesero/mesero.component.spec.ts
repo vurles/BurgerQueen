a39e40174df2b65e0ca78d9dfcb0c9afdabb3ab5
vurles <vurles27@gmail.com>
2019-05-15 13:51:43 -0400

    etapa 1 del tutorial angular

---
+++ burger-queen/src/app/mesero/mesero.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MeseroComponent } from './mesero.component';
+
+describe('MeseroComponent', () => {
+  let component: MeseroComponent;
+  let fixture: ComponentFixture<MeseroComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MeseroComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MeseroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
